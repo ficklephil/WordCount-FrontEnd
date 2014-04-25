4c5319d007ebeb4b4584773467342ad98404fbc6
Philip <philip.aberneithy@bskyb.com>
2014-04-25 16:27:18 +0100

    Filter on the Front End. Displays everything from Server. A bit of styling using BootStrap.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('wordCountFrontEndApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $http) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        //this needs to be a service
+        $http.get('http://localhost:3000/').success(function (data) {
+            console.log(data.article);
+            $scope.articles = data.article;
+        });
+
+    });
