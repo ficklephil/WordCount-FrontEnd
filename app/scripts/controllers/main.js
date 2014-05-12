fe48af65854647974aa42da6dddaa2eebffe1e1b
Philip <philip.aberneithy@bskyb.com>
2014-05-12 12:00:41 +0100

    Adding Loading articles message

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('wordCountFrontEndApp')
+angular.module('wordCountFrontEndApp',['cgBusy'])
     .controller('MainCtrl', function ($scope, $http) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
@@ -9,7 +9,7 @@ angular.module('wordCountFrontEndApp')
         ];
 
         //this needs to be a service
-        $http.get('http://localhost:3000/').success(function (data) {
+        $scope.myPromise = $http.get('http://localhost:3000/').success(function (data) {
             console.log(data.article);
             $scope.articles = data.article;
             $scope.searchDate = data.requestTime;
